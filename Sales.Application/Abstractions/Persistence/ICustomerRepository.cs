f235c091e0949e018c30830342e6af774bee8bec
isaqsanfer <isaelsferreira@outlook.com>
2026-07-26 22:01:04 -0300

    Adiciona interfaces de repositório para Category, Customer e Product

---
+++ Sales.Application/Abstractions/Persistence/ICustomerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sales.Domain.Customers.Entities;
+
+namespace Sales.Application.Abstractions.Persistence;
+
+public interface ICustomerRepository
+{
+    Task<Customer?> GetByIdAsync(Guid customerId, CancellationToken cancellationToken = default);
+    Task AddAsync(Customer customer, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Customer customer, CancellationToken cancellationToken = default);
+}
