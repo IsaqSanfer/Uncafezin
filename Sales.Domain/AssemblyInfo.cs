81812e31bf7fb39f3c39833d99c66e59b0832be0
isaqsanfer <isaelsferreira@outlook.com>
2026-07-28 22:55:54 -0300

    Permite acesso interno a Sales.Application e testes

--- Sales.Domain/AssemblyInfo.cs
+++ Sales.Domain/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 ﻿// Habilita a visualização dos membros internos para o projeto de testes "Sales.Domain.Tests"
 // Isso é útil para permitir que os testes acessem membros internos da biblioteca de domínio, sem expô-los publicamente
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Sales.Domain.Tests")]
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Sales.Domain.Tests")]
+[assembly: InternalsVisibleTo("Sales.Application")]
