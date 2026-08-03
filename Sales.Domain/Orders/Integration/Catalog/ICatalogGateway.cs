32e88fd4b0ed2e20ae6222b900ac1257ede46804
isaqsanfer <isaelsferreira@outlook.com>
2026-08-02 21:18:35 -0300

    Integração inicial com catálogo de produtos em pedidos
    Adicionados quatro arquivos para integração com o catálogo:
    - CatalogACL: ACL para acesso ao catálogo e validações.
    - ICatalogGateway: Interface para comunicação com o catálogo.
    - ProductDTO: DTO para transferência de dados do produto.
    - ProductSnapshot: ValueObject com validações de produto.

---
+++ Sales.Domain/Orders/Integration/Catalog/ICatalogGateway.cs
@@ -0,0 +1,7 @@
+﻿namespace Sales.Domain.Orders.Integration.Catalog;
+
+public interface ICatalogGateway
+{
+    Task<ProductDTO?> GetProductByIdAsync(Guid productId, CancellationToken cancellationToken = default);
+    Task<bool> GetAvailableStockAsync(Guid productId, int quantity, CancellationToken cancellationToken = default);
+}
